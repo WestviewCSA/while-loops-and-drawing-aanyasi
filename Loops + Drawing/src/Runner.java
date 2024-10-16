15cf964034048f0d78563baa94af7b1c5e440316
1917654 <1917654@01-18130.pusd.dom>
2024-10-16 10:34:33 -0700

    lops and drawings going to github

--- Loops + Drawing/src/Runner.java
+++ Loops + Drawing/src/Runner.java
@@ -13,6 +13,15 @@ public class Runner extends JPanel{
 	// methods
 	public void paint(Graphics g) {
 		
+		//make variable 
+		int lines = 30;
+		
+	
+//		//variable for random collor
+
+//		
+		
+		
 		Graphics2D g2 = (Graphics2D) g;
         g2.setStroke(new BasicStroke(5));
         
@@ -20,6 +29,7 @@ public class Runner extends JPanel{
 		//1) draw a concentric rectangle that's 10pixels from all sides of the "gray"
 		//portion of the GUI
 		g2.drawRect(10, 10, 765, 540);
+		
 		
 		
 		//2) Draw a horizontal line to bisect the Rectangle
@@ -33,6 +43,26 @@ public class Runner extends JPanel{
 		//4) small rectangles on top-left
 		g2.drawLine(765/4, 10, 765/4, 550/2-10);
 		
+		
+
+		//drawing colorful lines
+	while(lines<80) {
+		
+		int randColor = (int) (Math.random() * (255-1 + 1) ) + 1;
+		int randCol2 = (int) (Math.random() * (255-1 + 1) ) + 1;
+		int randCol3 = (int) (Math.random() * (255-1 + 1) ) + 1;
+		g.setColor( new Color(randColor, randCol2, randCol3));
+		
+		int rand = (int) (Math.random() * ((765 - 10) + 1) ) + 10;
+		int rand1 = (int) (Math.random() * ((765 - 10) + 1) ) + 10;
+		int rand2 = (int) (Math.random() * ((765 - 10) + 1) ) + 10;
+		int rand3 = (int) (Math.random() * ((765 - 10) + 1) ) + 10;
+			
+			g2.drawLine(rand, rand1, rand2, rand3);
+
+			//repeating code
+			lines++;
+		}
 		
 		/* 
 		 * int var = 0; 						<---- control variable
